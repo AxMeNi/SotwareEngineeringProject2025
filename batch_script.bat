4aac3e61bc1c07c959cce229d46b73b4f34ecf7c
AxMeNi <mengelle1u@etu.univ-lorraine.fr>
2025-09-19 13:35:36 +0200

    feat: add batch script to execute code

---
+++ batch_script.bat
@@ -0,0 +1,10 @@
+cd /d K:\S9\Software_engineering\software_engineering_project_2025\build
+
+cmake ..
+
+cmake --build .
+
+cmake --build . --target RUN_TESTS
+
+pause
+
--- tests/test-version.cpp
+++ tests/test-version.cpp
@@ -15,7 +15,7 @@ void test_version(){
     assert(buffer.str() == "project version is 1.0\n"); // test
 }
 
-// The test should pass if the main returns 0
+// The test should pass if the main returns 0, if 1 : doesnt pass
 
 int main() {
 	test_version();
