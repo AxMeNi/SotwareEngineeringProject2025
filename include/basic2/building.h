110d7c75be7ce855efea73db00681d56d3137653
AxMeNi <mengelle1u@etu.univ-lorraine.fr>
2025-09-23 10:22:45 +0200

    feat: added and modified new files for the new library

--- include/basic2/building.h
+++ include/basic2/building.h
@@ -2,33 +2,39 @@
 #define building_h
 
 #include <iostream>
-#include <dyn_array.h>
+#include <basic2/dyn_array.h>
 
 class Rock; // Forward declaration: Rock is a type, defined elsewhere
 
-class Building {
-	public:
-		/*!
-		 * Creates a Building with identifier and a number of 
-		 * rocks in its collection
-		 */
-		Building(int id, int nb_rocks);
-		Building();
-		Building(const Building& rhs); // CONSTRUCTEUR PAR COPIE
-		Building& operator=(const Building& rhs); // ASSIGNMENT OPERATOR
-		Building(const Building&& rhs): rocks_(rhs.rocks_.size()) //CONSTRUCTEUR DE MOVE
-		~Building();
+class Building
+{
+public:
+    /*!
+     * Creates a Building with identifier and a number of
+     * rocks in its collection
+     */
+    Building( int id, int nb_rocks );
+    Building() : id_( -1 )
+    {
+        std::cout << "Building::Building(), id_ = " << id_ << " at " << this
+                  << std::endl;
+    }
+    Building( const Building& rhs ) : id_( rhs.id_ )
+    {
+        std::cout << "Building::Building(" << &rhs << ", " << rhs.id_
+                  << ") id_ = " << id_ << " at " << this << std::endl;
+    }
+    Building& operator=( const Building& rhs );
+    ~Building();
 
-		const Rock* get_rock(int index) const;
-		bool swap_rock(
-				Building partner, 
-				unsigned int this_index, 
-				unsigned int partner_index
-		);
-	private:
-		friend std::ostream& operator<<(std::ostream& out, const Building& rhs);
-		int id_;
-		DynArray<Rock*> rocks_;
+    const Rock* get_rock( int index ) const;
+    bool swap_rock(
+        Building partner, unsigned int this_index, unsigned int partner_index );
+
+private:
+    friend std::ostream& operator<<( std::ostream& out, const Building& rhs );
+    int id_;
+    DynArray< Rock* > rocks_;
 };
 
 #endif 
--- mains/hello.cpp
+++ mains/hello.cpp
@@ -1,7 +1,5 @@
 #include <basic/message.h>
-#include <basic/dyn_array.h>
-#include <basic/building.h>
-#include <basic/rock.h>
+
 #include <iostream>
 
 int main()
@@ -14,26 +12,10 @@ int main()
     hello_world();
     print_version();
 
-	// EXECUTING THE CODE OF THE DYN ARRAY AND BUILDING PROJECT
-
-	std::cout << "#<hello.cpp> EXECUTION OF THE CODE FROM THE CLASS OF GUILLAUME" << std::endl;
-    DynArray< Building > street;
-	std::cout << "Welcome to SimTown! Please Enter IDs (-99 to finish)" << std::endl;
-	int id = -1;
-	std::cin >> id;
-	while(id != -99) {
-		Building tmp(id, 5);
-		street.push_back(tmp);
-		std::cin >> id;
-	}
-	for(unsigned int i = 0; i<street.size(); ++i ) {
-		std::cout << street[i] << std::endl;
-	}
-	if(street.size() >= 2 ) {
-		street[0].swap_rock(street[1], 2, 4);
-	}
 	std::cout << "End of program" << std::endl;
 
 	return 0;
 
 }
+
+
